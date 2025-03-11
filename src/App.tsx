0dd443b51f4731073361fab940e498f57d14bff5
gelo <gelodev83@gmail.com>
2025-03-11 16:02:05 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Navbar } from './components/Navbar/Navbar'
+import TopBar from './components/TopBar'
+
+function App() {
+ 
+
+  return (
+    <div className='font-inter'>
+    <Navbar/>
+    <TopBar/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/services/strapiService.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_REACT_APP_STRAPI_URL;
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: 5000,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export const fetchTopBar = async (endpoint: string) => {
+    const response = await api.get(`${endpoint}`);
+    return response.data;
+
+}
+
