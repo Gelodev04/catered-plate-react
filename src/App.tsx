3d7a1a79ea755e54ba1a475bdb81b337df4e7e46
gelo <gelodev83@gmail.com>
2025-03-13 17:04:01 +0800

    a

--- src/App.tsx
+++ src/App.tsx
@@ -18,19 +18,4 @@ function App() {
 
 export default App;
 
-import './App.css'
-import { Navbar } from './components/Navbar/Navbar'
-import TopBar from './components/TopBar'
 
-function App() {
- 
-
-  return (
-    <div className='font-inter'>
-    <Navbar/>
-    <TopBar/>
-    </div>
-  )
-}
-
-export default App
